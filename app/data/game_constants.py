3b019953c9a4c1abe3896e362bc915de12e53f9c
rainlash <rain.pipe.ash@gmail.com>
2020-03-17 19:18:42 -0500

    Added Heal Exp

--- app/data/game_constants.py
+++ app/data/game_constants.py
@@ -32,8 +32,8 @@ class ConstantCatalog(Data):
             base.value = value
 
 constants = ConstantCatalog([
-    Constant('num_items', "Maximum number of Items in inventory", int, 5),
-    Constant('num_accessories', "Maximum number of Accessories in inventory", int, 0),
+    Constant('num_items', "Max number of Items in inventory", int, 5),
+    Constant('num_accessories', "Max number of Accessories in inventory", int, 0),
     Constant('turnwheel', "Turnwheel", bool),
     Constant('overworld', "Overworld", bool),
     Constant('line_of_sight', "Force weapons to obey line of sight rules", bool),
@@ -43,10 +43,10 @@ constants = ConstantCatalog([
     Constant('enemy_leveling', "Method for autoleveling generic units", ("Random", "Fixed", "Hybrid", "FE8", "Match"), "Match"),
     Constant('rng', "Method for resolving accuracy rolls", ("Classic", "True Hit", "True Hit+", "Grandmaster"), "True Hit"),
     Constant('num_skills', "Expected number of Skills at max level", int, 5),
-    Constant('auto_promote', "Units will promote automatically upon reaching maximum level", bool),
-    Constant('min_damage', "Minimum damage dealt by an attack", int, 0),
+    Constant('auto_promote', "Units will promote automatically upon reaching max level", bool),
+    Constant('min_damage', "Min damage dealt by an attack", int, 0),
     Constant('boss_crit', "Final blow on boss will use critical animation", bool),
-    Constant('unarmed_disadvantage', "Amount of weapon disadvantage suffered by unarmed units", int, 0),
+    Constant('unarmed_disadvantage', "Unarmed weapon disadvantage multiplier", int, 0),
     Constant('convoy_on_death', "Weapons held by dead units are sent to convoy", bool),
     Constant('steal', "Steal Type", ("Nonweapons", "All unequipped"), "Nonweapons"),
     Constant('num_save_slots', "Number of save slots", int, 3),
@@ -54,6 +54,18 @@ constants = ConstantCatalog([
     Constant('attack_zero_dam', "Enemy AI attacks even if Damage is 0", bool, True),
     Constant('zero_move', "Treat Movement as 0 if AI does not move", bool, False),
     Constant('title', "Game Title", str, "Lex Talionis Game"),
-    Constant('kill_wexp', "Double weapon experience gained on kill", bool, True),
-    Constant('double_wexp', "Each hit when doubling grants weapon experience", bool, True),
-    Constant('miss_wexp', "Gain weapon experience even on miss", bool, True)])
+    Constant('kill_wexp', "Double weapon exp gained on kill", bool, True),
+    Constant('double_wexp', "Each hit when doubling grants weapon exp", bool, True),
+    Constant('miss_wexp', "Gain weapon exp even on miss", bool, True),
+    # Experience constants below
+    Constant('exp_curve', "How linear the exp curve is; Higher = less linear", 'ufloat', 0.035),
+    Constant('exp_magnitude', "How much base exp is received for each interaction", 'ufloat', 10),
+    Constant('exp_offset', "Tries to keep player character this many levels above enemies", float, 0),
+    Constant('kill_multiplier', "Exp multiplier on kill", 'ufloat', 3),
+    Constant('boss_bonus', "Extra exp for killing a boss", int, 40),
+    Constant('min_exp', "Min exp gained in combat", int, 1),
+    Constant('default_exp', "Default exp gained when nothing else applies", 'ufloat', 11),
+    Constant('steal_exp', "Exp gained for stealing an item", 'ufloat', 0),
+    Constant('heal_magnitude', "How much to multiply the amount healed by to determine experience gain", 'ufloat', 0),
+    Constant('heal_offset', "Added to total exp for healing", 'float', 11),
+    Constant('heal_min', "Min amount gained for healing", 'float', 11)])
--- app/editor/constant_database.py
+++ app/editor/constant_database.py
@@ -1,5 +1,7 @@
+import math
+
 from PyQt5.QtWidgets import QGridLayout, QLineEdit, QSpinBox, QHBoxLayout, \
-    QVBoxLayout, QGroupBox, QTreeView, QWidget
+    QVBoxLayout, QGroupBox, QTreeView, QWidget, QDoubleSpinBox, QLabel, QSizePolicy
 from PyQt5.QtCore import Qt, QAbstractItemModel
 
 from app.data.data import Data
@@ -42,6 +44,184 @@ class BoolConstantsModel(ComponentModel):
     def flags(self, index):
         basic_flags = Qt.ItemNeverHasChildren | Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsUserCheckable
         return basic_flags
+
+class MainExpEquation(QWidget):
+    def __init__(self, data, parent=None):
+        super().__init__(parent)
+        self.window = parent
+        self._data = data
+
+        label = QLabel('Combat Experience Equation:', self)
+        label.setAlignment(Qt.AlignBottom)
+        label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(label)
+
+        exp_magnitude = self._data.get('exp_magnitude')
+        self.exp_magnitude = QDoubleSpinBox(self)
+        self.exp_magnitude.setValue(exp_magnitude.value)
+        self.exp_magnitude.setDecimals(1)
+        self.exp_magnitude.setRange(0, 255)
+        self.exp_magnitude.valueChanged.connect(exp_magnitude.set_value)
+        exp_curve = self._data.get('exp_curve')
+        self.exp_curve = QDoubleSpinBox(self)
+        self.exp_curve.setValue(exp_curve.value)
+        self.exp_curve.setDecimals(3)
+        self.exp_curve.setRange(0, 255)
+        self.exp_curve.valueChanged.connect(exp_curve.set_value)
+        exp_offset = self._data.get('exp_offset')
+        self.exp_offset = QDoubleSpinBox(self)
+        self.exp_offset.setValue(exp_offset.value)
+        self.exp_offset.setDecimals(1)
+        self.exp_offset.setRange(-255, 255)
+        self.exp_offset.valueChanged.connect(exp_offset.set_value)
+
+        self.exp_magnitude.valueChanged.connect(self.window.parameters_changed)
+        self.exp_curve.valueChanged.connect(self.window.parameters_changed)
+        self.exp_offset.valueChanged.connect(self.window.parameters_changed)
+
+        label1 = QLabel(' * e^(', self)
+        label2 = QLabel(' * (<b>Level Difference</b> + ', self)
+        label3 = QLabel('))', self)
+
+        eq_layout = QHBoxLayout()
+        eq_layout.setAlignment(Qt.AlignHCenter)
+        eq_layout.setSpacing(0)
+        eq_layout.setContentsMargins(0, 0, 0, 0)
+        eq_layout.addWidget(self.exp_magnitude)
+        eq_layout.addWidget(label1)
+        eq_layout.addWidget(self.exp_curve)
+        eq_layout.addWidget(label2)
+        eq_layout.addWidget(self.exp_offset)
+        eq_layout.addWidget(label3)
+        layout.addLayout(eq_layout)
+
+class DisplayExpResults(QWidget):
+    def __init__(self, data, parent=None):
+        super().__init__(parent)
+        self.window = parent
+        self._data = data
+
+        self.level1 = QSpinBox(self)
+        self.level1.setValue(1)
+        self.level1.setRange(1, 255)
+        self.level1.setMaximumWidth(60)
+        self.level1.setAlignment(Qt.AlignRight)
+        self.level1.valueChanged.connect(self.update_parameters)
+        
+        self.level2 = QSpinBox(self)
+        self.level2.setValue(1)
+        self.level2.setRange(1, 255)
+        self.level2.setMaximumWidth(60)
+        self.level2.setAlignment(Qt.AlignRight)
+        self.level2.valueChanged.connect(self.update_parameters)
+
+        self.label1 = QLabel('A level ', self)
+        self.label2 = QLabel(' unit fights a level ', self)
+        self.label3 = QLabel(' unit', self)
+
+        self.label4 = QLabel('Experience Gained: ', self)
+        self.label4.setAlignment(Qt.AlignBottom)
+        self.label4.setAlignment(Qt.AlignRight)
+
+        self.edit_box = QLineEdit(self)
+        self.edit_box.setMaximumWidth(100)
+        self.edit_box.setReadOnly(True)
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        hlayout = QHBoxLayout()
+        hlayout.setAlignment(Qt.AlignHCenter)
+        hlayout.setSpacing(0)
+        hlayout.setContentsMargins(0, 0, 0, 0)
+        hlayout.addWidget(self.label1)
+        hlayout.addWidget(self.level1)
+        hlayout.addWidget(self.label2)
+        hlayout.addWidget(self.level2)
+        hlayout.addWidget(self.label3)
+        layout.addLayout(hlayout)
+
+        hlayout2 = QHBoxLayout()
+        hlayout2.setAlignment(Qt.AlignHCenter)
+        hlayout2.setSpacing(0)
+        hlayout2.setContentsMargins(0, 0, 0, 0)
+        hlayout2.addWidget(self.label4)
+        hlayout2.addWidget(self.edit_box)
+        layout.addLayout(hlayout2)
+
+        self.update_parameters()
+
+    # def resizeEvent(self, event):
+    #     print("Wow", flush=True)
+    #     super().resizeEvent(event)
+
+    #     self.label1.move(0, 0)
+    #     self.level1.move(self.label1.width(), 0)
+    #     running_width = self.label1.width() + self.level1.width()
+    #     self.label2.move(running_width, 0)
+    #     running_width += self.label2.width()
+    #     self.level2.move(running_width, 0)
+    #     running_width += self.level2.width()
+    #     self.label3.move(running_width, 0)
+
+    #     self.label4.move(0, self.label1.height() + 4)
+    #     self.edit_box.move(self.label4.width(), self.label1.height() + 4)
+
+    def update_parameters(self, val=None):
+        level_diff = self.level2.value() - self.level1.value() + self._data.get('exp_offset').value
+        exp_gained = self._data.get('exp_magnitude').value * math.exp(level_diff * self._data.get('exp_curve').value)
+        exp_gained = max(self._data.get('min_exp').value, exp_gained)
+        display = str(int(exp_gained)) + " (" + str(round(exp_gained, 2)) + ")"
+        self.edit_box.setText(display)
+
+class ExperienceWidget(QWidget):
+    def __init__(self, data, parent=None):
+        super().__init__(parent)
+        self.window = parent
+        self._data = data
+
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        self.main_exp_equation = MainExpEquation(data, self)
+        layout.addWidget(self.main_exp_equation, 0, 0, 1, 3)
+
+        min_exp = data.get('min_exp')
+        self.min_exp = PropertyBox(min_exp.name, QSpinBox, self)
+        self.min_exp.edit.setRange(0, 100)
+        self.min_exp.edit.setValue(min_exp.value)
+        self.min_exp.edit.setAlignment(Qt.AlignRight)
+        self.min_exp.edit.valueChanged.connect(min_exp.set_value)
+        self.min_exp.edit.valueChanged.connect(self.parameters_changed)
+
+        kill_multiplier = data.get('kill_multiplier')
+        self.kill_multiplier = PropertyBox(kill_multiplier.name, QDoubleSpinBox, self)
+        self.kill_multiplier.edit.setRange(0, 10)
+        self.kill_multiplier.edit.setDecimals(1)
+        self.kill_multiplier.edit.setAlignment(Qt.AlignRight)
+        self.kill_multiplier.edit.setValue(kill_multiplier.value)
+        self.kill_multiplier.edit.valueChanged.connect(kill_multiplier.set_value)
+        self.kill_multiplier.edit.valueChanged.connect(self.parameters_changed)
+
+        boss_bonus = data.get('boss_bonus')
+        self.boss_bonus = PropertyBox(boss_bonus.name, QSpinBox, self)
+        self.boss_bonus.edit.setRange(0, 255)
+        self.boss_bonus.edit.setAlignment(Qt.AlignRight)
+        self.boss_bonus.edit.setValue(boss_bonus.value)
+        self.boss_bonus.edit.valueChanged.connect(boss_bonus.set_value)
+        self.boss_bonus.edit.valueChanged.connect(self.parameters_changed)
+
+        layout.addWidget(self.min_exp, 1, 0)
+        layout.addWidget(self.kill_multiplier, 1, 1)
+        layout.addWidget(self.boss_bonus, 1, 2)
+
+        self.display_exp = DisplayExpResults(data, self)
+        layout.addWidget(self.display_exp, 2, 0, 1, 3)
+
+    def parameters_changed(self, val):
+        self.display_exp.update_parameters()
 
 class ConstantDatabase(DatabaseTab):
     @classmethod
@@ -90,9 +270,17 @@ class ConstantDatabase(DatabaseTab):
         misc_section = self.create_section(misc_constants)
         misc_section.setTitle("Miscellaneous Constants")
 
+        exp_section = QGroupBox(self)
+        exp_layout = QVBoxLayout()
+        exp_section.setLayout(exp_layout)
+        exp_widget = ExperienceWidget(self._data, self)
+        exp_layout.addWidget(exp_widget)
+        exp_section.setTitle("Combat Experience Constants")
+
         self.layout.addWidget(battle_section, 0, 0)
-        self.layout.addWidget(misc_section, 1, 0)
-        self.layout.addWidget(bool_section, 0, 1, 2, 1)
+        self.layout.addWidget(misc_section, 0, 1)
+        self.layout.addWidget(exp_section, 1, 0, 1, 2)
+        self.layout.addWidget(bool_section, 0, 2, 2, 1)
 
     def create_section(self, constants):
         section = QGroupBox(self)

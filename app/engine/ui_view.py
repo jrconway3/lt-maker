8f1b7564490a28024fae1bc15cda6c5a672dfd80
rainlash <rain.pipe.ash@gmail.com>
2021-06-02 17:57:16 -0500

    Fix crash when viewing item description panel of a unit with no portrait

--- app/engine/ui_view.py
+++ app/engine/ui_view.py
@@ -691,15 +691,18 @@ class ItemDescriptionPanel():
         if not self.surf:
             self.surf = self.create_surf()
 
+        cursor_left = False
+        if game.cursor.position[0] > TILEX // 2 + game.camera.get_x():
+            topleft = (WINWIDTH - 8 - self.surf.get_width(), WINHEIGHT - 8 - self.surf.get_height())
+        else:
+            cursor_left = True
+            topleft = (8, WINHEIGHT - 8 - self.surf.get_height())
+
         portrait = icons.get_portrait(self.unit)
         if portrait:
-            if game.cursor.position[0] > TILEX // 2 + game.camera.get_x():
-                topleft = (WINWIDTH - 8 - self.surf.get_width(), WINHEIGHT - 8 - self.surf.get_height())
-                surf.blit(portrait, (topleft[0] + 2, topleft[1] - 76))
-            else:
-                topleft = (8, WINHEIGHT - 8 - self.surf.get_height())
+            if cursor_left:
                 portrait = engine.flip_horiz(portrait)
-                surf.blit(portrait, (topleft[0] + 2, topleft[1] - 76))
-
+            surf.blit(portrait, (topleft[0] + 2, topleft[1] - 76))
+                
         surf.blit(self.surf, topleft)
         return surf

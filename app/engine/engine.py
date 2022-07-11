ad966b61fc42b439df1a93629810b0bd86b21191
kdports <kdports@gmail.com>
2022-07-11 17:27:31 -0400

    Try-except fullscreen catch

--- app/engine/engine.py
+++ app/engine/engine.py
@@ -35,7 +35,10 @@ def set_title(text):
     pygame.display.set_caption(text)
 
 def build_display(size):
-    return pygame.display.set_mode(size, pygame.FULLSCREEN if cf.SETTINGS['fullscreen'] else 0)
+    try:
+        return pygame.display.set_mode(size, pygame.FULLSCREEN if cf.SETTINGS['fullscreen'] else 0)
+    except:
+        return pygame.display.set_mode(size)
 
 def push_display(surf, size, new_surf):
     pygame.transform.scale(surf, size, new_surf)

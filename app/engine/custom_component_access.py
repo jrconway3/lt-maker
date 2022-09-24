c67d1971e6ac4040357ea20d7a36a1b7c48e4aa3
mag <bill.tang.03.25@gmail.com>
2022-09-24 10:14:00 -0500

    error message on bad custom components

--- app/engine/custom_component_access.py
+++ app/engine/custom_component_access.py
@@ -1,3 +1,8 @@
+import logging
+import traceback
+from PyQt5.QtWidgets import QMessageBox
+
+
 current_custom_path = None
 def get_components() -> bool:
     # For getting custom project components at runtime
@@ -17,11 +22,20 @@ def get_components() -> bool:
     if module_path != current_custom_path and os.path.exists(module_path):
         current_custom_path = module_path
         print("Importing Custom Components")
-        spec = importlib.util.spec_from_file_location('custom_components', module_path)
-        module = importlib.util.module_from_spec(spec)
-        # spec.name is 'custom_components'
-        sys.modules[spec.name] = module
-        spec.loader.exec_module(module)
+        try:
+            spec = importlib.util.spec_from_file_location('custom_components', module_path)
+            module = importlib.util.module_from_spec(spec)
+            # spec.name is 'custom_components'
+            sys.modules[spec.name] = module
+            spec.loader.exec_module(module)
+        except:
+            import_failure_msg = traceback.format_exc()
+            logging.error("Failed to import custom components: %s" % (import_failure_msg))
+            error_msg = QMessageBox()
+            error_msg.setIcon(QMessageBox.Critical)
+            error_msg.setText("Failed to import custom components. \n\n" + import_failure_msg)
+            error_msg.setWindowTitle("Components Fatal Error")
+            error_msg.exec_()
 
     return os.path.exists(module_path)
 

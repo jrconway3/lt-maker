c63d250efa3724d87d675a81f104057ac9057ae8
mag <bill.tang.03.25@gmail.com>
2022-11-27 22:32:57 -0600

    added generic typing to property box for ease of use

--- app/extensions/custom_gui.py
+++ app/extensions/custom_gui.py
@@ -1,3 +1,4 @@
+from typing import Generic, Type, TypeVar
 from PyQt5.QtWidgets import QSpinBox, QComboBox, QDialog, QWidget, QHBoxLayout, \
     QLineEdit, QPushButton, QAction, QMenu, QSizePolicy, QFrame, \
     QDialogButtonBox, QListView, QTreeView, QTableView, QItemDelegate, QLabel, QVBoxLayout, QApplication, \
@@ -110,8 +111,10 @@ class LineSearch(QWidget):
         layout.addWidget(self.search_button)
         self.setLayout(layout)
 
-class PropertyBox(QWidget):
-    def __init__(self, label, widget, parent=None, horiz_layout=False):
+T = TypeVar('T')
+
+class PropertyBox(QWidget, Generic[T]):
+    def __init__(self, label, widget: Type[T], parent=None, horiz_layout=False):
         super().__init__(parent)
         self.window = parent
 
@@ -132,7 +135,7 @@ class PropertyBox(QWidget):
             self.label.setAlignment(Qt.AlignBottom)
         size_policy = QSizePolicy.Preferred
         self.label.setSizePolicy(size_policy, QSizePolicy.Fixed)
-        self.edit = widget(self)
+        self.edit: T = widget(self)
         self.edit.setSizePolicy(size_policy, QSizePolicy.Fixed)
         self.bottom_section = QHBoxLayout()
         self.bottom_section.addWidget(self.edit)

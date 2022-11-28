ec4fcbf34e7f9f17783ff360f7ddd801bf2ffc5e
mag <bill.tang.03.25@gmail.com>
2022-11-27 22:22:30 -0600

    nid edit will autosub _

--- app/editor/lib/components/validated_line_edit.py
+++ app/editor/lib/components/validated_line_edit.py
@@ -1,3 +1,4 @@
+import re
 from PyQt5.QtWidgets import QPushButton, QLineEdit
 from PyQt5.QtCore import QSize, QRegExp
 from PyQt5.QtGui import QRegExpValidator
@@ -8,13 +9,17 @@ from app.data.database.database import DB
 
 from app.extensions.custom_gui import PropertyBox, ComboBox
 
+class BetterRegExpValidator(QRegExpValidator):
+  def fixup(self, a0: str) -> str:
+    return re.sub("[^A-Za-z0-9_]", '_', a0)
+
 class NidLineEdit(QLineEdit):
   """For strict limiting on NID forms. Alphanumerics and underscore only.
   """
   def __init__(self, *args, **kwargs) -> None:
     super().__init__(*args, **kwargs)
     reg_ex = QRegExp(r"[A-Za-z0-9_]*")
-    input_validator = QRegExpValidator(reg_ex, self)
+    input_validator = BetterRegExpValidator(reg_ex, self)
     self.setValidator(input_validator)
 
 class NoParentheticalLineEdit(QLineEdit):

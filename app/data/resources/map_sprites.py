267835ac628e021ff20b1598ce74c3c0236c807d
mag <bill.tang.03.25@gmail.com>
2022-12-26 22:42:33 -0600

    i hate windows

--- app/data/resources/map_sprites.py
+++ app/data/resources/map_sprites.py
@@ -1,13 +1,15 @@
 import os
 import shutil
+from typing import Optional
 
 from app.data.resources.base_catalog import ManifestCatalog
+from app.utilities.typing import NID
 
 class MapSprite():
     def __init__(self, nid, stand_full_path=None, move_full_path=None):
-        self.nid = nid
-        self.stand_full_path = stand_full_path
-        self.move_full_path = move_full_path
+        self.nid: NID = nid
+        self.stand_full_path: Optional[str] = stand_full_path
+        self.move_full_path: Optional[str] = move_full_path
         self.standing_image = None
         self.moving_image = None
         self.standing_pixmap = None
@@ -45,12 +47,18 @@ class MapSpriteCatalog(ManifestCatalog[MapSprite]):
             # Stand sprite
             new_full_path = os.path.join(loc, map_sprite.nid + '-stand.png')
             if os.path.abspath(map_sprite.stand_full_path) != os.path.abspath(new_full_path):
-                shutil.copy(map_sprite.stand_full_path, new_full_path)
+                try:
+                    shutil.copy(map_sprite.stand_full_path, new_full_path)
+                except shutil.SameFileError: # windows filesystem doesn't distinguish between capitals
+                    os.rename(map_sprite.stand_full_path, new_full_path)
                 map_sprite.set_stand_full_path(new_full_path)
             # Move sprite
             new_full_path = os.path.join(loc, map_sprite.nid + '-move.png')
             if os.path.abspath(map_sprite.move_full_path) != os.path.abspath(new_full_path):
-                shutil.copy(map_sprite.move_full_path, new_full_path)
+                try:
+                    shutil.copy(map_sprite.move_full_path, new_full_path)
+                except shutil.SameFileError:
+                    os.rename(map_sprite.move_full_path, new_full_path)
                 map_sprite.set_move_full_path(new_full_path)
         self.dump(loc)
 

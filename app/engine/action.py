80bd339e243a0950ad501956f7ecc93f776fb6a9
rainlash <rainlash>
2023-02-10 09:27:30 -0700

    not sure how this can happen, but fixed time_left decrement

--- app/engine/action.py
+++ app/engine/action.py
@@ -2570,10 +2570,10 @@ class DecrementTimeRegion(Action):
         self.region = region
 
     def do(self):
-        self.region.time_left = self.region.time_left - 1
+        self.region.time_left = int(self.region.time_left) - 1
 
     def reverse(self):
-        self.region.time_left = self.region.time_left + 1
+        self.region.time_left = int(self.region.time_left) + 1
 
 class RemoveRegion(Action):
     def __init__(self, region):

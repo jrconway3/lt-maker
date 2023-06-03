7254020e30e8d75d09d1f8d26220480b19908f7e
rainlash <rainlash>
2023-06-03 14:21:57 -0600

    also on BACK

--- app/engine/game_menus/menu_states/unit_menu_state.py
+++ app/engine/game_menus/menu_states/unit_menu_state.py
@@ -62,7 +62,10 @@ class UnitMenuState(State):
                 selected = self.ui_display.cursor_hover()
                 if isinstance(selected, UnitObject):
                     if self.in_level:
-                        game.cursor.set_pos(selected.position)
+                        if selected.position:
+                            game.cursor.set_pos(selected.position)
+                        elif game.get_rescuers_position(selected):
+                            game.cursor.set_pos(game.get_rescuers_position(selected))
             game.state.change('transition_pop')
 
         elif event == 'SELECT':

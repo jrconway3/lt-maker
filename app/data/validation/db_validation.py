ed92235e3afcdf6dd6be3f72bc12a4f84c50af78
magdalene <saintessmagdalena@gmail.com>
2023-10-17 21:17:26 -0400

    fix a dict bug

--- app/data/validation/db_validation.py
+++ app/data/validation/db_validation.py
@@ -1,71 +1,86 @@
-
+from __future__ import annotations
 from ast import Tuple
 from typing import Any, List
+
 from app.data.database.components import ComponentType
 from app.data.database.database import Database
+from app.data.database.database_types import DatabaseType
 from app.data.database.database_validator import DatabaseValidatorEngine
+from app.data.resources.resource_types import ResourceType
 from app.data.resources.resources import Resources
-from app.data.validation.validation_errors import ItemComponentValidationError, SkillComponentValidationError
+from app.data.validation.validation_errors import (
+    ComponentValidationError, ItemComponentValidationError, LevelValidationError, SkillComponentValidationError)
 from app.events.python_eventing.preprocessor import Preprocessor
+
+
+def validate_levels(database: Database, resources: Resources) -> List:
+    validator = DatabaseValidatorEngine(database, resources)
+    all_errors = []
+    for level in database.levels:
+        def _validate(dtype, attr, can_be_null=True):
+            val = getattr(level, attr)
+            if not val and can_be_null:
+                return
+            if not validator.validate(dtype, val):
+                all_errors.append(LevelValidationError(level.nid, attr, val, dtype))
+        # _validate(ResourceType.TILEMAPS, 'tilemap')
+        # _validate(ResourceType.TILEMAPS, 'bg_tilemap')
+        # _validate(DatabaseType.PARTIES, 'party')
+    return all_errors
 
 
 def validate_items_and_skills(database: Database, resources: Resources) -> List:
     validator = DatabaseValidatorEngine(database, resources)
+    def _validate_component_type(obj_name: str, component_name: str, ctype: ComponentType | Tuple[ComponentType, ComponentType], cval, is_item=False) -> List[ComponentValidationError]:
+        if is_item:
+            error_cls = ItemComponentValidationError
+        else:
+            error_cls = SkillComponentValidationError
+        types_and_values = []
+        if isinstance(ctype, tuple):
+            container = ctype[0]
+            value_type = ctype[1]
+            if container == ComponentType.List:
+                # list of nids
+                types_and_values = [(value_type, value) for value in cval]
+            elif container in (ComponentType.Dict, ComponentType.FloatDict):
+                # see elsewhere for usages: always dict of 'nid: int'
+                types_and_values = [(value_type, key) for key, _ in cval]
+            elif container == ComponentType.MultipleChoice:
+                # short-circuit. we don't use DB validation for this
+                if not cval in value_type:
+                    return [error_cls(obj_name, component_name, value_type, cval)]
+        else:
+            types_and_values = [(ctype, cval)]
+
+        errors = []
+        for ctype, cvalue in types_and_values:
+            if not validator.validate(ctype, cvalue):
+                errors.append(error_cls(obj_name, component_name, ctype, cvalue))
+        return errors
+
     all_errors = []
     for skill in database.skills:
         for component in skill.components:
             # attribute components convey info by being present; they don't have values
             if not component.expose:
                 continue
-            types_and_values: List[Tuple[ComponentType, Any]] = []
             if component.expose == ComponentType.NewMultipleOptions:
-                types_and_values = [(component.options[option], component.value[option]) for option in component.options.keys()]
-            elif isinstance(component.expose, tuple):
-                container = component.expose[0]
-                value_type = component.expose[1]
-                if container == ComponentType.List:
-                    # list of nids
-                    types_and_values = [(value_type, value) for value in component.value]
-                elif container in (ComponentType.Dict, ComponentType.FloatDict):
-                    # see elsewhere for usages: always dict of 'nid: int'
-                    types_and_values = [(value_type, key) for key, _ in component.value]
-                elif container == ComponentType.MultipleChoice:
-                    # short-circuit. we don't use DB validation for this
-                    if not component.value in value_type:
-                        all_errors.append(SkillComponentValidationError(skill.nid, component.name, value_type, component.value))
-                    continue
+                for option in component.options.keys():
+                    all_errors += _validate_component_type(skill.nid, component.name, component.options[option], component.value[option])
             else:
-                types_and_values = [(component.expose, component.value)]
-            for ctype, cvalue in types_and_values:
-                if not validator.validate(ctype, cvalue):
-                    all_errors.append(SkillComponentValidationError(skill.nid, component.name, ctype, cvalue))
+                all_errors += _validate_component_type(skill.nid, component.name, component.expose, component.value)
+
     for item in database.items:
         for component in item.components:
             # attribute components convey info by being present; they don't have values
             if not component.expose:
                 continue
-            types_and_values: List[Tuple[ComponentType, Any]] = []
             if component.expose == ComponentType.NewMultipleOptions:
-                types_and_values = [(component.options[option], component.value[option]) for option in component.options.keys()]
-            elif isinstance(component.expose, tuple):
-                container = component.expose[0]
-                value_type = component.expose[1]
-                if container == ComponentType.List:
-                    # list of nids
-                    types_and_values = [(value_type, value) for value in component.value]
-                elif container in (ComponentType.Dict, ComponentType.FloatDict):
-                    # see elsewhere for usages: always dict of 'nid: int'
-                    types_and_values = [(value_type, key) for key, _ in component.value]
-                elif container == ComponentType.MultipleChoice:
-                    # short-circuit. we don't use DB validation for this
-                    if not component.value in value_type:
-                        all_errors.append(ItemComponentValidationError(item.nid, component.name, value_type, component.value))
-                    continue
+                for option in component.options.keys():
+                    all_errors += _validate_component_type(item.nid, component.name, component.options[option], component.value[option], True)
             else:
-                types_and_values = [(component.expose, component.value)]
-            for ctype, cvalue in types_and_values:
-                if not validator.validate(ctype, cvalue):
-                    all_errors.append(ItemComponentValidationError(item.nid, component.name, ctype, cvalue))
+                all_errors += _validate_component_type(item.nid, component.name, component.expose, component.value, True)
     return all_errors
 
 def validate_events(database: Database, resources: Resources) -> List:
@@ -79,5 +94,6 @@ def validate_events(database: Database, resources: Resources) -> List:
 def validate_all(database: Database, resources: Resources) -> List:
     event_errors = validate_events(database, resources)
     skill_errors = validate_items_and_skills(database, resources)
-    all_errors = event_errors + skill_errors
+    level_errors = validate_levels(database, resources)
+    all_errors = event_errors + skill_errors + level_errors
     return all_errors
--- app/data/validation/validation_errors.py
+++ app/data/validation/validation_errors.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
 from app.data.database.components import ComponentType
+from app.data.database.database_types import DatabaseType
+from app.data.resources.resource_types import ResourceType
 from app.events.python_eventing.errors import ERROR_TEMPLATE
 from app.utilities.typing import NID
 
@@ -28,3 +31,26 @@ class ItemComponentValidationError(ComponentValidationError):
 
 class SkillComponentValidationError(ComponentValidationError):
     object_type = "Skill"
+
+
+@dataclass
+class ObjectFieldValidationError():
+    nid: NID
+    field_name: str
+    field_value: str
+    field_type: ResourceType | DatabaseType
+    object_type = "Unknown"
+    error_template = \
+"""
+    In {type} '{nid}':
+        Error in field {field_name}:
+    No {field_type} with name '{field_value}'
+"""
+
+    def __str__(self) -> str:
+        return self.error_template.format(type=self.object_type, nid=self.nid,
+                                          field_name=self.field_name, field_value=str(self.field_value),
+                                          field_type=self.field_type.name)
+
+class LevelValidationError(ObjectFieldValidationError):
+    object_type = "Level"

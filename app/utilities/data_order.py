88aa54fc777862fe228bc791615bf86afb30fd3d
magdalene <saintessmagdalena@gmail.com>
2024-02-03 21:26:57 -0500

    remove other migration code

--- app/utilities/data_order.py
+++ app/utilities/data_order.py
@@ -10,13 +10,5 @@ def rchop(s: str, suffix: str):
 def parse_order_keys_file(fname: Path) -> List[str]:
     with open(fname) as load_file:
         okeys = json.load(load_file)
-    # TODO(mag): this is migration code; remove it on 1/1/2024
-    if isinstance(okeys, dict):
-        ordering = okeys.keys()
-        ordering = [rchop(f, '.json') for f in ordering]
-        ordering = sorted(ordering, key=lambda k: okeys.get(k, 99999))
-        return ordering
-    # correct code
-    elif isinstance(okeys, list):
         okeys = [rchop(f, '.json') for f in okeys]
         return okeys

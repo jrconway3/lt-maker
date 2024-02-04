17bac9c959b67369cacee0b6c9b8192565b5d116
magdalene <saintessmagdalena@gmail.com>
2024-02-03 21:32:37 -0500

    fix tests

--- app/tests/test_event_commands.py
+++ app/tests/test_event_commands.py
@@ -96,7 +96,7 @@ class EventCommandUnitTests(unittest.TestCase):
         command3, bad_idx = event_commands.parse_text_to_command("set_current_hp;Seth;13;no_warn", strict=True)
         self.assertEqual(command3.chosen_flags, {"no_warn"})
         # Test with display values (use of parentheses)
-        command4, bad_idx = event_commands.parse_text_to_command("set_current_hp;Seth (Seth);13", strict=True)
+        command4, bad_idx = event_commands.parse_text_to_command("set_current_hp;Seth;13", strict=True)
         self.assertEqual(command4.parameters.get('Unit'), "Seth")
         self.assertEqual(command4.parameters.get('HP'), "13")
         # Test with evaluables
@@ -168,7 +168,7 @@ class EventCommandUnitTests(unittest.TestCase):
         self.assertEqual(parameters, {"Speaker": "Seth", "Speed": 5.5})
         self.assertTrue(len(flags) == 0)
         # Test with flags and exising function
-        command3, _ = event_commands.parse_text_to_command("give_item;Eirika;Rapier;FLAG(no_banner)", strict=True)
+        command3, _ = event_commands.parse_text_to_command("give_item;Eirika;Rapier;no_banner", strict=True)
         self.assertTrue(isinstance(command3, event_commands.GiveItem))
         self.assertEqual(len(command3.chosen_flags), 1)
         self.assertEqual(command3.chosen_flags, {"no_banner"})

120c46c8132fe68d9ada373707d771bbd9029d00
Squid1003 <jeffrey314271@gmail.com>
2025-05-01 17:04:16 +0000

    Add target_ai item component.

--- app/engine/item_components/ai_components.py
+++ app/engine/item_components/ai_components.py
@@ -7,4 +7,39 @@ class NoAI(ItemComponent):
 
     def ai_priority(self, unit, item, target, move):
         return -1
+
+class EvalAIPriority(ItemComponent):
+    nid = 'eval_ai_priority'
+    desc = """Gives a condition under which the AI will target units with this item.  Higher priority makes the AI more likely to use this item.
+    Priority values should typically be between 0 and 1, but values above 1 can be used to more strongly encourage an action."""
+    tag = ItemTags.UTILITY
+    expose = ComponentType.NewMultipleOptions
+    options = {
+        'condition': ComponentType.String,
+        'priority': ComponentType.String
+    }
+
+    def __init__(self, value=None):
+        self.value = {
+            'condition': 'True',
+            'priority': '0'
+        }
+        if value:
+            self.value.update(value)
+    
+    def ai_priority(self, unit, item, target, move):
+        if target:
+            from app.engine import evaluate
+            try:
+                condition: bool = bool(evaluate.evaluate(self.value['condition'], unit, target, local_args={'item': item, 'move': move}))
+            except Exception as e:
+                logging.error("eval_ai_priority failed to evaluate condition %s with error %s", self.value['condition'], e)
+                return 0
+            if condition:
+                    try:
+                        priority_term: float = float(evaluate.evaluate(self.value['priority'], unit, target, local_args={'item': item, 'move': move}))
+                        return priority_term
+                    except Exception as e:
+                        logging.error("eval_ai_priority failed to evaluate priority %s with error %s", self.value['priority'], e)
+        return 0
     

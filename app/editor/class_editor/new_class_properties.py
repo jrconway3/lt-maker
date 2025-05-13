86002deb06a5b32544b1e55d1cabee624ac0cbf7
JaidynReiman <jaidyn@jrconway.net>
2025-05-13 03:26:49 +0000

    * Added Folders to Class Tab Using NewEditorTab

--- app/editor/class_editor/new_class_properties.py
+++ app/editor/class_editor/new_class_properties.py
@@ -23,6 +23,7 @@ from app.editor.weapon_editor.weapon_rank import WexpGainDelegate, WexpGainMulti
 from app.editor.learned_skill_delegate import LearnedSkillDelegate
 from app.editor.icons import ItemIcon80
 from app.editor.lib.components.validated_line_edit import NidLineEdit
+from app.editor.component_editor_properties import T
 
 from app.editor.class_editor import class_model
 from app.editor.map_sprite_editor import map_sprite_tab
@@ -31,13 +32,23 @@ from app.editor.combat_animation_editor import combat_animation_tab
 from app.editor import timer
 
 from app.utilities import str_utils
+from app.utilities.typing import NID
 
-class ClassProperties(QWidget):
-    def __init__(self, parent, current=None):
+from typing import (Callable, Optional)
+
+class NewClassProperties(QWidget):
+    title = "Class"
+
+    def __init__(self, parent, current: Optional[T] = None,
+                 attempt_change_nid: Optional[Callable[[NID, NID], bool]] = None,
+                 on_icon_change: Optional[Callable] = None):
         super().__init__(parent)
         self.window = parent
-        self.model = self.window.left_frame.model
-        self._data = self.window._data
+
+        self.current: Optional[T] = current
+        self.cached_nid: Optional[NID] = self.current.nid if self.current else None
+        self.attempt_change_nid = attempt_change_nid
+        self.on_icon_change = on_icon_change
 
         self.settings = MainSettingsController()
         theme = dark_theme.get_theme()
@@ -206,21 +217,31 @@ class ClassProperties(QWidget):
         timer.get_timer().tick_elapsed.connect(self.tick)
 
     def tick(self):
-        self.window.update_list()
+        self.on_icon_change()
+
+    def on_icon_changed(self, nid):
+        if self.current:
+            self.current.icon_nid = nid
+            if self.on_icon_change:
+                self.on_icon_change()
 
     def nid_changed(self, text):
-        self.current.nid = text
-        self.window.update_list()
+        if self.current:
+            # Also change name if they are identical
+            if self.current.name == self.current.nid.replace('_', ' '):
+                self.name_box.edit.setText(text.replace('_', ' '))
+            self.current.nid = text
 
     def nid_done_editing(self):
-        # Check validity of nid!
-        other_nids = [d.nid for d in self._data.values() if d is not self.current]
-        if self.current.nid in other_nids:
-            QMessageBox.warning(self.window, 'Warning', 'Class ID %s already in use' % self.current.nid)
-            self.current.nid = str_utils.get_next_name(self.current.nid, other_nids)
-        self.model.on_nid_changed(self._data.find_key(self.current), self.current.nid)
-        self._data.update_nid(self.current, self.current.nid)
-        self.window.update_list()
+        if self.current and self.cached_nid:
+            self.nid_box.edit.blockSignals(True)  # message box causes focus loss which double triggers nid_done_editing
+            # Check validity of nid!
+            if self.attempt_change_nid and self.attempt_change_nid(self.cached_nid, self.current.nid):
+                self.cached_nid = self.current.nid
+            else:
+                self.current.nid = self.cached_nid
+                self.nid_box.edit.setText(self.cached_nid)
+            self.nid_box.edit.blockSignals(False)
 
     def name_changed(self, text):
         self.current.name = text
@@ -295,7 +316,7 @@ class ClassProperties(QWidget):
             self.current.map_sprite_nid = nid
             pix = class_model.get_map_sprite_icon(self.current, num=0)
             self.map_sprite_label.setPixmap(pix)
-            self.window.update_list()
+            self.on_icon_change()
 
     def autoselect_map_sprite(self):
         nid = self.current.nid
@@ -305,7 +326,7 @@ class ClassProperties(QWidget):
             self.current.map_sprite_nid = nid
             pix = class_model.get_map_sprite_icon(self.current, num=0)
             self.map_sprite_label.setPixmap(pix)
-            self.window.update_list()
+            self.on_icon_change()
 
     def select_combat_anim(self):
         res, ok = combat_animation_tab.get_animations()
@@ -315,7 +336,6 @@ class ClassProperties(QWidget):
             pix = class_model.get_combat_anim_icon(self.current)
             if pix:
                 self.combat_anim_label.setPixmap(pix)
-            self.window.update_list()
         else:  # Use to clear the combat animation -- since this can be reasonable
             self.current.combat_anim_nid = None
             self.combat_anim_label.clear()
@@ -329,57 +349,61 @@ class ClassProperties(QWidget):
             pix = class_model.get_combat_anim_icon(self.current)
             if pix:
                 self.combat_anim_label.setPixmap(pix)
-            self.window.update_list()
 
     def set_current(self, current):
-        self.current = current
-        self.nid_box.edit.setText(current.nid)
-        self.name_box.edit.setText(current.name)
-        self.desc_box.edit.setText(current.desc)
-        self.tier_box.edit.setValue(current.tier)
-        self.max_level_box.edit.setValue(current.max_level)
-        self.movement_box.edit.setValue(current.movement_group)
-        # Reset promotes from box
-        self.promotes_from_box.edit.clear()
-        self.promotes_from_box.edit.addItems(["None"] + DB.classes.keys())
-        if current.promotes_from:
-            self.promotes_from_box.edit.setValue(current.promotes_from)
+        if not current:
+            self.setEnabled(False)
         else:
-            self.promotes_from_box.edit.setValue("None")
-        # Need to make copies because otherwise ResetSelection calls
-        # self.tag_box.updated which resets the current.tags
-        turns_into = current.turns_into[:]
-        tags = current.tags[:]
-        self.turns_into_box.edit.clear()
-        self.turns_into_box.edit.addItems(DB.classes.keys())
-        self.turns_into_box.edit.setCurrentTexts(turns_into)
-        self.tag_box.edit.clear()
-        self.tag_box.edit.addItems(DB.tags.keys())
-        self.tag_box.edit.setCurrentTexts(tags)
+            self.setEnabled(True)
+            self.current = current
+            self.cached_nid = current.nid
+            self.nid_box.edit.setText(current.nid)
+            self.name_box.edit.setText(current.name)
+            self.desc_box.edit.setText(current.desc)
+            self.tier_box.edit.setValue(current.tier)
+            self.max_level_box.edit.setValue(current.max_level)
+            self.movement_box.edit.setValue(current.movement_group)
+            # Reset promotes from box
+            self.promotes_from_box.edit.clear()
+            self.promotes_from_box.edit.addItems(["None"] + DB.classes.keys())
+            if current.promotes_from:
+                self.promotes_from_box.edit.setValue(current.promotes_from)
+            else:
+                self.promotes_from_box.edit.setValue("None")
+            # Need to make copies because otherwise ResetSelection calls
+            # self.tag_box.updated which resets the current.tags
+            turns_into = current.turns_into[:]
+            tags = current.tags[:]
+            self.turns_into_box.edit.clear()
+            self.turns_into_box.edit.addItems(DB.classes.keys())
+            self.turns_into_box.edit.setCurrentTexts(turns_into)
+            self.tag_box.edit.clear()
+            self.tag_box.edit.addItems(DB.tags.keys())
+            self.tag_box.edit.setCurrentTexts(tags)
 
-        self.class_stat_widget.update_stats()
-        self.class_stat_widget.set_new_obj(current)
-        if self.averages_dialog:
-            self.averages_dialog.set_current(current)
+            self.class_stat_widget.update_stats()
+            self.class_stat_widget.set_new_obj(current)
+            if self.averages_dialog:
+                self.averages_dialog.set_current(current)
 
-        self.class_skill_widget.set_current(current.learned_skills)
-        default_weapons = {weapon_nid: DB.weapons.default(DB) for weapon_nid in DB.weapons.keys()}
-        default_weapons.update(current.wexp_gain)
-        current.wexp_gain = default_weapons
-        self.wexp_gain_widget.set_current(current.wexp_gain)
-        self.field_widget.set_current(current.fields)
+            self.class_skill_widget.set_current(current.learned_skills)
+            default_weapons = {weapon_nid: DB.weapons.default(DB) for weapon_nid in DB.weapons.keys()}
+            default_weapons.update(current.wexp_gain)
+            current.wexp_gain = default_weapons
+            self.wexp_gain_widget.set_current(current.wexp_gain)
+            self.field_widget.set_current(current.fields)
 
-        self.icon_edit.set_current(current.icon_nid, current.icon_index)
-        pix = class_model.get_map_sprite_icon(self.current, num=0)
-        if pix:
-            self.map_sprite_label.setPixmap(pix)
-        else:
-            self.map_sprite_label.clear()
-        pix = class_model.get_combat_anim_icon(self.current)
-        if pix:
-            self.combat_anim_label.setPixmap(pix)
-        else:
-            self.combat_anim_label.clear()
+            self.icon_edit.set_current(current.icon_nid, current.icon_index)
+            pix = class_model.get_map_sprite_icon(self.current, num=0)
+            if pix:
+                self.map_sprite_label.setPixmap(pix)
+            else:
+                self.map_sprite_label.clear()
+            pix = class_model.get_combat_anim_icon(self.current)
+            if pix:
+                self.combat_anim_label.setPixmap(pix)
+            else:
+                self.combat_anim_label.clear()
 
     def hideEvent(self, event):
         self.close_averages()

6c9079cef4719d08e2067f3e7d482806de9a25d8
David Conway Jr. <david@trailercentral.com>
2025-06-25 18:46:57 -0500

    * Fixed Missed Changes

--- app/editor/combat_animation_editor/new_palette_properties.py
+++ app/editor/combat_animation_editor/new_palette_properties.py
@@ -556,17 +556,12 @@ class NewPaletteProperties(QWidget):
 
     def nid_changed(self, text):
         self.current_palette.nid = text
-        self.window.update_list()
 
     def nid_done_editing(self):
         # Check validity of nid!
-        other_nids = [d.nid for d in self._data.values() if d is not self.current_palette]
-        if self.current_palette.nid in other_nids:
-            QMessageBox.warning(self.window, 'Warning', 'Palette ID %s already in use' % self.current_palette.nid)
-            self.current_palette.nid = str_utils.get_next_name(self.current_palette.nid, other_nids)
-        self.model.on_nid_changed(self._data.find_key(self.current_palette), self.current_palette.nid)
-        self._data.update_nid(self.current_palette, self.current_palette.nid)
-        self.window.update_list()
+        if not self.attempt_change_nid(self.cached_nid, self.current_palette.nid):
+            self.current_palette.nid = self.cached_nid
+        self.cached_nid = self.current_palette.nid
 
     @property
     def current(self):

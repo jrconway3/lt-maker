4333b9e46c911a4d58d052a8b8451a7bf4edf637
JaidynReiman <jaidyn@jaidynreiman.net>
2026-03-25 22:04:52 -0500

    * Fixed Extra Merge Issue

--- app/engine/item_components/usable_components.py
+++ app/engine/item_components/usable_components.py
@@ -561,7 +561,7 @@ class ManaCostAsUses(ItemComponent):
         return None
 
     def item_uses_display(self, unit, item) -> UsesDisplayConfig:
-        return UsesDisplayConfig(self._calc_uses, self.delim, self._calc_max_uses, self._font_color, unit=unit, item=item)
+        return UsesDisplayConfig(self._calc_uses, get_uses_color=self._font_color, unit=unit, item=item)
 
 class RemainingManaUses(ManaCostAsUses):
     nid = 'remaining_mana_uses'

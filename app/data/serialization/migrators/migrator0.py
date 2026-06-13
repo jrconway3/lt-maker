2b81a3ba945096e3675d108cce9ca121ac2e8c27
Dung <dangleha3008@gmail.com>
2026-06-13 21:54:07 +0800

    Fix fail migration still made edits to png files

--- app/data/serialization/migrators/migrator0.py
+++ app/data/serialization/migrators/migrator0.py
@@ -21,31 +21,39 @@ class Migrator0(MigratorBase):
                         os.path.join('.', data_dir, 'old_portraits'),
                         dirs_exist_ok=True)
         filetype = '.png'
-        for portrait in portraits:
-            # Update PNG files
-            fn = os.path.join(data_dir, 'portraits', portrait.get('nid') + filetype)
-            im = engine.image_load(fn)
-            face_frame = im.subsurface((0, 0, 96, 80))
-            blink_frames = im.subsurface((96, 48, 32, 32))
-            mouth_frames = im.subsurface((0, 80, 128, 32))
-            minimug = im.subsurface((96, 16, 32, 32))
 
-            new_im = engine.create_simple_surface((160, 112))
-            new_im.fill(COLORKEY)
-            new_im.blit(face_frame, (32, 0))
-            new_im.blit(blink_frames, (128, 48))
-            new_im.blit(mouth_frames, (0, 80))
-            new_im.blit(minimug, (128, 80))
-            engine.save_surface(new_im, fn)
+        try:
+            for portrait in portraits:
+                # Update PNG files
+                fn = os.path.join(data_dir, 'portraits', portrait.get('nid') + filetype)
+                im = engine.image_load(fn)
+                face_frame = im.subsurface((0, 0, 96, 80))
+                blink_frames = im.subsurface((96, 48, 32, 32))
+                mouth_frames = im.subsurface((0, 80, 128, 32))
+                minimug = im.subsurface((96, 16, 32, 32))
 
-            # Update data
-            portrait['info_offset'] = (8, portrait.get('info_offset'))
-            portrait['chibi_coord'] = (128, 80)
-            portrait['full_size'] = (160, 112)
-            portrait['face_size'] = (96, 80)
-            portrait['blink_size'] = (32, 16)
-            portrait['mouth_size'] = (32, 16)
-            portrait['blink_frames'] = 2
-            portrait['mouth_frames'] = 3
+                new_im = engine.create_simple_surface((160, 112))
+                new_im.fill(COLORKEY)
+                new_im.blit(face_frame, (32, 0))
+                new_im.blit(blink_frames, (128, 48))
+                new_im.blit(mouth_frames, (0, 80))
+                new_im.blit(minimug, (128, 80))
+                engine.save_surface(new_im, fn)
+
+                # Update data
+                portrait['info_offset'] = (8, portrait.get('info_offset'))
+                portrait['chibi_coord'] = (128, 80)
+                portrait['full_size'] = (160, 112)
+                portrait['face_size'] = (96, 80)
+                portrait['blink_size'] = (32, 16)
+                portrait['mouth_size'] = (32, 16)
+                portrait['blink_frames'] = 2
+                portrait['mouth_frames'] = 3
+        except:
+            # Bring back the old portrait to undo the edits made to PNG files
+            shutil.copytree(os.path.join('.', data_dir, 'old_portraits'),
+                            os.path.join('.', data_dir, 'portraits'),
+                            dirs_exist_ok=True)
+            raise
 
         return resource_dict
